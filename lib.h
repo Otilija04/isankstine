d113424b9965313c9bb40528c55cd253b14c26ae
Otilija04 <145570265+Otilija04@users.noreply.github.com>
2024-01-03 00:34:51 +0200

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,11 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+std::string removePunctuation(const std::string& word);
+std::vector<std::string> extractURLs(const std::string& text);
+
+#endif // LIB_H
